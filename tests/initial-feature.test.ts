3c91184def679e16851a96652e9879650495f902
Peter Strøiman <peter@stroiman.com>
2024-06-03 10:10:03 +0200

    Change editor config to 2 space indentation.

--- tests/initial-feature.test.ts
+++ tests/initial-feature.test.ts
@@ -3,17 +3,17 @@ import { expect } from "chai";
 import { foo } from "../src/implementation";
 
 describe("Developing some feature", () => {
-	it("Should start with a failing test", () => {
-		expect(foo()).to.equal(42);
-	});
+  it("Should start with a failing test", () => {
+    expect(foo()).to.equal(42);
+  });
 });
 
 describe("Examples of verifying mocking", () => {
-	it("Has has sinon verification setup", () => {
-		const spy = sinon.spy();
-		spy(42);
-		expect(spy).to.have.been.calledOnceWith(42);
-		// Enable the chai.should() call in setup.ts to allow this syntax
-		// spy.should.have.been.calledOnceWith(42)
-	});
+  it("Has has sinon verification setup", () => {
+    const spy = sinon.spy();
+    spy(42);
+    expect(spy).to.have.been.calledOnceWith(42);
+    // Enable the chai.should() call in setup.ts to allow this syntax
+    // spy.should.have.been.calledOnceWith(42)
+  });
 });
