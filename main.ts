1a2dcbad58ccad5bd9f524d9832301f060cd6fe5
Peter Strøiman <peter@stroiman.com>
2024-06-03 14:42:29 +0200

    Comment out stuff we don't need

--- main.ts
+++ main.ts
@@ -29,30 +29,30 @@ export default class MyPlugin extends Plugin {
   async onload() {
     await this.loadSettings();
 
-    // This creates an icon in the left ribbon.
-    const ribbonIconEl = this.addRibbonIcon(
-      "dice",
-      "Sample Plugin",
-      (evt: MouseEvent) => {
-        // Called when the user clicks the icon.
-        new Notice("This is a notice!");
-      },
-    );
-    // Perform additional things with the ribbon
-    ribbonIconEl.addClass("my-plugin-ribbon-class");
+    // // This creates an icon in the left ribbon.
+    // const ribbonIconEl = this.addRibbonIcon(
+    //   "dice",
+    //   "Sample Plugin",
+    //   (evt: MouseEvent) => {
+    //     // Called when the user clicks the icon.
+    //     new Notice("This is a notice!");
+    //   },
+    // );
+    // // Perform additional things with the ribbon
+    // ribbonIconEl.addClass("my-plugin-ribbon-class");
 
     // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
     const statusBarItemEl = this.addStatusBarItem();
     statusBarItemEl.setText("Status Bar Text");
 
     // This adds a simple command that can be triggered anywhere
-    this.addCommand({
-      id: "open-sample-modal-simple",
-      name: "Open sample modal (simple)",
-      callback: () => {
-        new SampleModal(this.app).open();
-      },
-    });
+    // this.addCommand({
+    //   id: "open-sample-modal-simple",
+    //   name: "Open sample modal (simple)",
+    //   callback: () => {
+    //     new SampleModal(this.app).open();
+    //   },
+    // });
     // This adds an editor command that can perform some operation on the current editor instance
     this.addCommand({
       id: "publish-article",
@@ -76,25 +76,25 @@ export default class MyPlugin extends Plugin {
       },
     });
     // This adds a complex command that can check whether the current state of the app allows execution of the command
-    this.addCommand({
-      id: "open-sample-modal-complex",
-      name: "Open sample modal (complex)",
-      checkCallback: (checking: boolean) => {
-        // Conditions to check
-        const markdownView =
-          this.app.workspace.getActiveViewOfType(MarkdownView);
-        if (markdownView) {
-          // If checking is true, we're simply "checking" if the command can be run.
-          // If checking is false, then we want to actually perform the operation.
-          if (!checking) {
-            new SampleModal(this.app).open();
-          }
-
-          // This command will only show up in Command Palette when the check function returns true
-          return true;
-        }
-      },
-    });
+    // this.addCommand({
+    //   id: "open-sample-modal-complex",
+    //   name: "Open sample modal (complex)",
+    //   checkCallback: (checking: boolean) => {
+    //     // Conditions to check
+    //     const markdownView =
+    //       this.app.workspace.getActiveViewOfType(MarkdownView);
+    //     if (markdownView) {
+    //       // If checking is true, we're simply "checking" if the command can be run.
+    //       // If checking is false, then we want to actually perform the operation.
+    //       if (!checking) {
+    //         new SampleModal(this.app).open();
+    //       }
+    //
+    //       // This command will only show up in Command Palette when the check function returns true
+    //       return true;
+    //     }
+    //   },
+    // });
 
     // This adds a settings tab so the user can configure various aspects of the plugin
     this.addSettingTab(new SampleSettingTab(this.app, this));
