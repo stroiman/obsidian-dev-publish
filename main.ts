d59813f9ca86c2279522343b1d5489e94b643416
Peter Strøiman <peter@stroiman.com>
2024-06-03 17:06:57 +0200

    Add settings option for API key

--- main.ts
+++ main.ts
@@ -16,11 +16,11 @@ import Publisher from "src/publisher";
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-  mySetting: string;
+  apiKey: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-  mySetting: "default",
+  apiKey: "",
 };
 
 export default class MyPlugin extends Plugin {
@@ -29,66 +29,16 @@ export default class MyPlugin extends Plugin {
   async onload() {
     await this.loadSettings();
 
-    // // This creates an icon in the left ribbon.
-    // const ribbonIconEl = this.addRibbonIcon(
-    //   "dice",
-    //   "Sample Plugin",
-    //   (evt: MouseEvent) => {
-    //     // Called when the user clicks the icon.
-    //     new Notice("This is a notice!");
-    //   },
-    // );
-    // // Perform additional things with the ribbon
-    // ribbonIconEl.addClass("my-plugin-ribbon-class");
-
-    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-    const statusBarItemEl = this.addStatusBarItem();
-    statusBarItemEl.setText("Status Bar Text");
-
-    // This adds a simple command that can be triggered anywhere
-    // this.addCommand({
-    //   id: "open-sample-modal-simple",
-    //   name: "Open sample modal (simple)",
-    //   callback: () => {
-    //     new SampleModal(this.app).open();
-    //   },
-    // });
-    // This adds an editor command that can perform some operation on the current editor instance
     this.addCommand({
-      id: "publish-article",
+      id: "publish-current-note",
       name: "Publish current note to dev.to",
-      editorCallback: (editor: Editor, view: MarkdownView) => {
-        if (!view.file) {
-          return;
-        }
-        const publisher = new Publisher(
-          this.app.fileManager,
-          new MediumGateway("", requestUrl),
-          this.app.vault,
-          new GetFrontMatterInfo(),
-        );
-        publisher.publish(view.file).then(
-          () => {
-            console.log("Stuff published to dev.to");
-          },
-          (err) => console.error("Error publishing to dev.to"),
-        );
-      },
-    });
-    // This adds a complex command that can check whether the current state of the app allows execution of the command
-    this.addCommand({
-      id: "open-sample-modal-complex",
-      name: "Open sample modal (complex)",
       checkCallback: (checking: boolean) => {
-        // Conditions to check
         const view = this.app.workspace.getActiveViewOfType(MarkdownView);
         if (view && view.file) {
-          // If checking is true, we're simply "checking" if the command can be run.
-          // If checking is false, then we want to actually perform the operation.
           if (!checking) {
             const publisher = new Publisher(
               this.app.fileManager,
-              new MediumGateway("", requestUrl),
+              new MediumGateway(this.settings.apiKey, requestUrl),
               this.app.vault,
               new GetFrontMatterInfo(),
             );
@@ -99,8 +49,6 @@ export default class MyPlugin extends Plugin {
               (err) => console.error("Error publishing to dev.to"),
             );
           }
-
-          // This command will only show up in Command Palette when the check function returns true
           return true;
         }
       },
@@ -132,22 +80,6 @@ export default class MyPlugin extends Plugin {
   }
 }
 
-class SampleModal extends Modal {
-  constructor(app: App) {
-    super(app);
-  }
-
-  onOpen() {
-    const { contentEl } = this;
-    contentEl.setText("Woah!");
-  }
-
-  onClose() {
-    const { contentEl } = this;
-    contentEl.empty();
-  }
-}
-
 class SampleSettingTab extends PluginSettingTab {
   plugin: MyPlugin;
 
@@ -162,14 +94,16 @@ class SampleSettingTab extends PluginSettingTab {
     containerEl.empty();
 
     new Setting(containerEl)
-      .setName("Setting #1")
-      .setDesc("It's a secret")
+      .setName("API KEY")
+      .setDesc(
+        "SECURITY WARNING! This will be stored unencrypted in your obsidian plugin folder. Do not use this plugin if you do not fully understand the security implications of this.",
+      )
       .addText((text) =>
         text
           .setPlaceholder("Enter your secret")
-          .setValue(this.plugin.settings.mySetting)
+          .setValue(this.plugin.settings.apiKey)
           .onChange(async (value) => {
-            this.plugin.settings.mySetting = value;
+            this.plugin.settings.apiKey = value;
             await this.plugin.saveSettings();
           }),
       );
