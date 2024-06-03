650225840984e6e3c2cd4439b0e5cd6476647950
Peter Strøiman <peter@stroiman.com>
2024-06-03 16:52:31 +0200

    Make command more accessible.

--- main.ts
+++ main.ts
@@ -76,25 +76,35 @@ export default class MyPlugin extends Plugin {
       },
     });
     // This adds a complex command that can check whether the current state of the app allows execution of the command
-    // this.addCommand({
-    //   id: "open-sample-modal-complex",
-    //   name: "Open sample modal (complex)",
-    //   checkCallback: (checking: boolean) => {
-    //     // Conditions to check
-    //     const markdownView =
-    //       this.app.workspace.getActiveViewOfType(MarkdownView);
-    //     if (markdownView) {
-    //       // If checking is true, we're simply "checking" if the command can be run.
-    //       // If checking is false, then we want to actually perform the operation.
-    //       if (!checking) {
-    //         new SampleModal(this.app).open();
-    //       }
-    //
-    //       // This command will only show up in Command Palette when the check function returns true
-    //       return true;
-    //     }
-    //   },
-    // });
+    this.addCommand({
+      id: "open-sample-modal-complex",
+      name: "Open sample modal (complex)",
+      checkCallback: (checking: boolean) => {
+        // Conditions to check
+        const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (view && view.file) {
+          // If checking is true, we're simply "checking" if the command can be run.
+          // If checking is false, then we want to actually perform the operation.
+          if (!checking) {
+            const publisher = new Publisher(
+              this.app.fileManager,
+              new MediumGateway("", requestUrl),
+              this.app.vault,
+              new GetFrontMatterInfo(),
+            );
+            publisher.publish(view.file).then(
+              () => {
+                console.log("Stuff published to dev.to");
+              },
+              (err) => console.error("Error publishing to dev.to"),
+            );
+          }
+
+          // This command will only show up in Command Palette when the check function returns true
+          return true;
+        }
+      },
+    });
 
     // This adds a settings tab so the user can configure various aspects of the plugin
     this.addSettingTab(new SampleSettingTab(this.app, this));
