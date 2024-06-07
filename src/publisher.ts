e9682060d98f807ca09ff9b5acfef27683a9ae47
Peter Strøiman <peter@stroiman.com>
2024-06-07 13:32:28 +0200

    Extract code to helper functions
    This makes the actual data flow stand out more.

--- src/publisher.ts
+++ src/publisher.ts
@@ -30,16 +30,13 @@ export default class Publisher<TFile> {
     this.getFrontMatterInfo = getFrontMatterInfo;
   }
 
-  async publish(file: TFile) {
-    const mediumId = await new Promise((resolve, reject) =>
-      this.fileManager
-        .processFrontMatter(file, (frontmatter) => {
-          const mediumId = frontmatter[ARTICLE_ID_KEY];
-          // TODO: What if it's not a number?
-          resolve(mediumId);
-        })
-        .catch((err) => reject(err)),
-    );
+  async getFrontMatter(file: TFile) {
+    return new Promise<any>((resolve, reject) => {
+      this.fileManager.processFrontMatter(file, resolve).catch(reject);
+    });
+  }
+
+  async getArticleData(file: TFile) {
     const fileContents = await this.vault.read(file);
     const metadataCache = this.app.metadataCache.getFileCache(file);
     const h1 = metadataCache?.headings?.find((x) => x.level === 1);
@@ -48,22 +45,39 @@ export default class Publisher<TFile> {
       : fileContents;
     const frontmatterInfo =
       await this.getFrontMatterInfo.getFrontMatterInfo(dataAfterHeading);
-    const markdown = frontmatterInfo.exists
-      ? dataAfterHeading.substring(frontmatterInfo.contentStart)
-      : dataAfterHeading;
-    const article = {
+    const markdown = (
+      frontmatterInfo.exists
+        ? dataAfterHeading.substring(frontmatterInfo.contentStart)
+        : dataAfterHeading
+    ).trim();
+    return {
       title: h1?.heading || "Heading Missing",
-      markdown: markdown.trim(),
+      markdown,
     };
+  }
+
+  async updateFrontmatter(file: TFile, newData: any) {
+    await this.fileManager.processFrontMatter(file, (existing) => {
+      for (const key of Object.keys(newData)) {
+        existing[key] = newData[key];
+      }
+    });
+  }
+
+  async publish(file: TFile) {
+    const frontmatter = await this.getFrontMatter(file);
+    const mediumId = frontmatter[ARTICLE_ID_KEY];
+    const article = await this.getArticleData(file);
     if (typeof mediumId === "number") {
       await this.gateway.updateArticle({ id: mediumId, article });
     } else {
       const result = await this.gateway.createArticle({ article });
-      this.fileManager.processFrontMatter(file, (frontmatter) => {
-        frontmatter[ARTICLE_ID_KEY] = result.id;
-        frontmatter[ARTICLE_URL_KEY] = result.url;
-        frontmatter[ARTICLE_CANONICAL_URL_KEY] = result.canonicalUrl;
-      });
+      const newFrontmatter = {
+        [ARTICLE_ID_KEY]: result.id,
+        [ARTICLE_URL_KEY]: result.url,
+        [ARTICLE_CANONICAL_URL_KEY]: result.canonicalUrl,
+      };
+      this.updateFrontmatter(file, newFrontmatter);
     }
   }
 }
