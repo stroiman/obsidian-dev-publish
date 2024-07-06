de5bf021b348eb802c4948c21aba4de80eb59d47
Peter Strøiman <peter@stroiman.com>
2024-07-06 12:41:25 +0200

    Create a command to trigger image mapping

--- src/image-mapping-dialog.ts
+++ src/image-mapping-dialog.ts
@@ -15,7 +15,7 @@ const createTable = (element: HTMLElement, list: EmbeddedImageMapping[]) => {
   {
     const tr = thead.createEl("tr");
     tr.createEl("th", { text: "Image" });
-    tr.createEl("th", { text: "Public URLx" });
+    tr.createEl("th", { text: "Public URL" });
   }
   const tbody = table.createEl("tbody");
   for (const item of list) {
@@ -40,23 +40,23 @@ const showDialog = (
 ): Promise<null | ImageMappingList> => {
   return new Promise((resolve) => {
     let submitting = false;
-    const result = new Modal(app);
-    result.onClose = function () {
+    const dialog = new Modal(app);
+    dialog.onClose = function () {
       if (!submitting) {
         resolve(null);
       }
     };
-    result.setTitle("Map image links");
-    result.contentEl.createEl("p", {
+    dialog.setTitle("Map image links");
+    dialog.contentEl.createEl("p", {
       text: "Image embeds cannot be uploaded to DEV automatically. You need to make them available, e.g. by uploading manually on DEV, or through some other service. Once done, you can provide the mapping here.",
     });
-    const form = result.contentEl.createEl("form");
+    const form = dialog.contentEl.createEl("form");
     const { inputs } = createTable(form, list);
-    form.createEl("button", { text: "Submit" });
+    form.createEl("button", { type: "submit", text: "Submit" });
     form.addEventListener("submit", (e) => {
       e.preventDefault();
       submitting = true;
-      form.close();
+      dialog.close();
       resolve(
         list.map((x) => ({
           imageFile: x.imageFile,
@@ -64,7 +64,7 @@ const showDialog = (
         })),
       );
     });
-    result.open();
+    dialog.open();
   });
 };
 
@@ -72,7 +72,7 @@ export type ShowImageMappingDialog = (
   imageMappingList: ImageMappingList,
 ) => Promise<ImageMappingList | null>;
 
-class DialogController {
+export class TheDialogController {
   private app: App;
 
   constructor(app: App) {
@@ -80,10 +80,12 @@ class DialogController {
   }
 
   showImageMappingDialog(list: ImageMappingList) {
-    return showDialog(app, list);
+    return showDialog(this.app, list);
   }
 }
 
-export type { DialogController };
+export type DialogController = {
+  showImageMappingDialog: TheDialogController["showImageMappingDialog"];
+};
 
 export default { showDialog };
